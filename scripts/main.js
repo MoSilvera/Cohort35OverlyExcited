4fbee445f5232aac7fa43bdc18680d3136ca08b0
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-08-26 13:50:59 -0500

    adding third argument

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,21 @@ let sentence = ["The","walrus","danced","through","the","trees","in","the","ligh
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray, punctuation) {
+function addExcitement (theWordArray, punctuation, number) {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
+    symbol = punctuation
+    for (let index = 1 ; index < number; index++) {
+        symbol = punctuation + symbol
+
+    }
 
     for (let i = 0; i < theWordArray.length; i++) {
         // Concatenate the new word onto buildMeUp
+
         if((i +1) % 3 === 0){
-            buildMeUp = buildMeUp + " " + theWordArray[i] + punctuation
+            buildMeUp = buildMeUp + " " + theWordArray[i] + symbol
         }
         else{
             buildMeUp = buildMeUp + " " + theWordArray[i]
@@ -26,4 +32,4 @@ function addExcitement (theWordArray, punctuation) {
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence, "👾" )
+addExcitement(sentence, "👾", 4 )
